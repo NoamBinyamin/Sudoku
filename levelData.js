b1f16447603348592207992a09d3137070c4e204
NoamBinyamin <noam151315@gmail.com>
2025-07-28 15:50:34 +0300

    added more levels

--- levelData.js
+++ levelData.js
@@ -287,6 +287,87 @@ const levels = {
     ],
   },
   7: {
+    name: "Level 7",
+    difficulty: "Death 🚑",
+    cellValues: [
+      // FIRST ROW
+      //CELL-1
+      { cellID: "0-0-0-0", cellValue: 1 },
+      { cellID: "0-0-1-1", cellValue: 3 },
+      { cellID: "0-0-2-2", cellValue: 9 },
+      //CELL-2
+      { cellID: "0-1-0-2", cellValue: 7 },
+      { cellID: "0-1-1-1", cellValue: 2 },
+      { cellID: "0-1-2-0", cellValue: 6 },
+      //CELL-3
+      { cellID: "0-2-0-1", cellValue: 9 },
+      { cellID: "0-2-1-2", cellValue: 8 },
+      { cellID: "0-2-2-0", cellValue: 5 },
+      // SECOND ROW
+      //CELL-1
+      { cellID: "1-0-0-2", cellValue: 5 },
+      { cellID: "1-0-1-1", cellValue: 1 },
+      { cellID: "1-0-2-0", cellValue: 6 },
+
+      //CELL-2
+      { cellID: "1-1-0-0", cellValue: 3 },
+      { cellID: "1-1-1-1", cellValue: 8 },
+      { cellID: "1-1-2-2", cellValue: 4 },
+      //CELL-3
+      { cellID: "1-2-0-0", cellValue: 9 },
+      { cellID: "1-2-1-1", cellValue: 2 },
+      // THIRD ROW
+      //CELL-1
+      { cellID: "2-0-0-0", cellValue: 3 },
+      { cellID: "2-0-1-1", cellValue: 4 },
+      { cellID: "2-0-2-2", cellValue: 7 },
+      //CELL-2
+      //CELL-3
+      { cellID: "2-2-0-1", cellValue: 1 },
+      { cellID: "2-2-1-2", cellValue: 7 },
+      { cellID: "2-2-2-0", cellValue: 3 },
+    ],
+  },
+  8: {
+    name: "Level 8",
+    difficulty: "Impossible 🏳️",
+    cellValues: [
+      // FIRST ROW
+      //CELL-1
+      { cellID: "0-0-1-1", cellValue: 6 },
+      { cellID: "0-0-2-2", cellValue: 8 },
+      //CELL-2
+      { cellID: "0-1-0-0", cellValue: 1 },
+      { cellID: "0-1-0-2", cellValue: 2 },
+      //CELL-3
+      { cellID: "0-2-1-1", cellValue: 7 },
+      { cellID: "0-2-2-0", cellValue: 9 },
+      // SECOND ROW
+      //CELL-1
+      { cellID: "1-0-0-0", cellValue: 4 },
+      { cellID: "1-0-1-1", cellValue: 5 },
+      { cellID: "1-0-2-0", cellValue: 2 },
+
+      //CELL-2
+      { cellID: "1-1-1-2", cellValue: 7 },
+      { cellID: "1-1-2-1", cellValue: 8 },
+      //CELL-3
+      { cellID: "1-2-0-2", cellValue: 3 },
+      { cellID: "1-2-2-2", cellValue: 1 },
+      // THIRD ROW
+      //CELL-1
+      { cellID: "2-0-0-2", cellValue: 9 },
+      { cellID: "2-0-1-1", cellValue: 7 },
+      //CELL-2
+      { cellID: "2-1-2-0", cellValue: 3 },
+      { cellID: "2-1-2-2", cellValue: 4 },
+      //CELL-3
+      { cellID: "2-2-0-0", cellValue: 8 },
+      { cellID: "2-2-1-1", cellValue: 6 },
+      { cellID: "2-2-0-2", cellValue: 5 },
+    ],
+  },
+  9: {
     name: "Test Level",
     difficulty: "Noam's Tests",
     cellValues: [
